b776f71eceff1165aceb14f61efde87ed99df395
joshu <boeitniet@nree.nl>
2024-10-21 12:08:18 +0200

    swipe test working !!!!

--- app/src/main/java/com/example/kippie/MainActivity.kt
+++ app/src/main/java/com/example/kippie/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.kippie
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    internal var x1: Float = 0.toFloat()
+    internal var x2: Float = 0.toFloat()
+    internal var y1: Float = 0.toFloat()
+    internal var y2: Float = 0.toFloat()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onTouchEvent(tochevent: MotionEvent): Boolean {
+        when (tochevent.action) {
+            MotionEvent.ACTION_DOWN -> {
+                x1 = tochevent.x
+                y1 = tochevent.y
+            }
+            MotionEvent.ACTION_UP -> {
+                x2 = tochevent.x
+                y2 = tochevent.y
+                if (x1 < x2) {
+                    val i = Intent(this@MainActivity, scherm2::class.java)
+                    startActivity(i)
+                }
+            }
+        }
+        return false
+    }
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +50,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         findViewById<Button>(R.id.button).setOnClickListener{
-        val i = Intent(this@MainActivity, scherm2::class.java)
-        startActivity(i)
+        val d = Intent(this@MainActivity, scherm2::class.java)
+        startActivity(d)
         }
     }
-}
+}}
