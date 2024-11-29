b1fb537ac04b576231382e2110c0bc6d925e49a6
joshu <boeitniet@nree.nl>
2024-11-29 14:35:04 +0100

    made text into buttons, need to add variables for order

--- app/src/main/java/com/example/kippie/Kippiepannen.kt
+++ app/src/main/java/com/example/kippie/Kippiepannen.kt
@@ -24,49 +24,49 @@ class Kippiepannen : AppCompatActivity() {
             hapjespan.visibility = View.VISIBLE
             val hapjespan2 = findViewById<View>(R.id.antikluif2)
             hapjespan2.visibility = View.VISIBLE
-            val hapjespan3 = findViewById<View>(R.id.antikluiftext)
+            val hapjespan3 = findViewById<View>(R.id.antikluifbutton2)
             hapjespan3.visibility = View.VISIBLE
-            val hapjespan4 = findViewById<View>(R.id.antikluiftext2)
+            val hapjespan4 = findViewById<View>(R.id.antikluifbutton)
             hapjespan4.visibility = View.VISIBLE
         }
         fun loadmaaltijd() {
            val maaltijd = findViewById<View>(R.id.maaltijdpan)
            maaltijd.visibility = View.VISIBLE
-            val maaltijd2 = findViewById<View>(R.id.maaltijdpantext)
-            maaltijd2.visibility = View.VISIBLE
             val maaltijd3 = findViewById<View>(R.id.nasipan)
             maaltijd3.visibility = View.VISIBLE
-            val maaltijd4 = findViewById<View>(R.id.nasipantext)
+            val maaltijd2 = findViewById<View>(R.id.maaltijdpanbutton)
+            maaltijd2.visibility = View.VISIBLE
+            val maaltijd4 = findViewById<View>(R.id.nasipanbutton)
             maaltijd4.visibility = View.VISIBLE
         }
         fun loadvlees() {
             val vlees = findViewById<View>(R.id.satepanketjap)
             vlees.visibility = View.VISIBLE
-            val vlees2 = findViewById<View>(R.id.satepanketjaptext)
-            vlees2.visibility = View.VISIBLE
             val vlees3 = findViewById<View>(R.id.kippelingpan)
             vlees3.visibility = View.VISIBLE
-                val vlees4 = findViewById<View>(R.id.kippelingtext)
+            val vlees2 = findViewById<View>(R.id.satepanketjapbutton)
+            vlees2.visibility = View.VISIBLE
+            val vlees4 = findViewById<View>(R.id.Kippelingpanbutton)
             vlees4.visibility = View.VISIBLE
         }
         fun loadvega() {
            val vega = findViewById<View>(R.id.imageaardappel)
            vega.visibility = View.VISIBLE
-            val vega2 = findViewById<View>(R.id.aardappelpan)
-            vega2.visibility = View.VISIBLE
             val vega3 = findViewById<View>(R.id.maaltijdpan)
             vega3.visibility = View.VISIBLE
-            val vega4 = findViewById<View>(R.id.maaltijdpantext)
+            val vega4 = findViewById<View>(R.id.aardappelpanbutton)
             vega4.visibility = View.VISIBLE
+            val vega2 = findViewById<View>(R.id.maaltijdpanbutton)
+            vega2.visibility = View.VISIBLE
         }
         fun loadXXL() {
             val xxl = findViewById<View>(R.id.knakpanXxl)
             xxl.visibility = View.VISIBLE
-            val xxl2 = findViewById<View>(R.id.knakpanxxltext)
-            xxl2.visibility = View.VISIBLE
             val xxl3 = findViewById<View>(R.id.snackpanXxl)
             xxl3.visibility = View.VISIBLE
-            val xxl4 = findViewById<View>(R.id.snackpanXxltext)
+            val xxl2 = findViewById<View>(R.id.knakpanXxlbutton)
+            xxl2.visibility = View.VISIBLE
+            val xxl4 = findViewById<View>(R.id.SnackpanXxlbutton)
             xxl4.visibility = View.VISIBLE
         }
 
